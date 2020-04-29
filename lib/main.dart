a1d92c6da58403f88ac9ce0617a7a176e56cb158
shay <shaysha6@gmail.com>
2020-04-30 01:38:07 +0300

    Shay 1.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'input_page.dart';
 
-//Shay
+//Shay 1.0
 void main() => runApp(BMICalculator());
 
 class BMICalculator extends StatelessWidget {
@@ -9,7 +9,7 @@ class BMICalculator extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark().copyWith(
-        primaryColor: Color(0xFF241A3C),
+        primaryColor: Color(0xFF141A3C),
       ),
       home: InputPage(),
     );
